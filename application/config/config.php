8bb0e2e95ebe7f833a750adc98ee594ded0c2280
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2024-07-10 12:55:53 +0700

    Base URL Tweak

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://kuroakevizago.my.id/';
+$config['base_url'] = 'https://kuroakevizago.my.id/';
 
 /*
 |--------------------------------------------------------------------------
